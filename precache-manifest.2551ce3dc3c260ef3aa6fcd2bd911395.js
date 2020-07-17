d0139848e5ae6b8648256e95138d6bf312a155af
Ngapasi <ahmadfauzi2526@gmail.com>
2020-07-18 03:15:46 +0700

    Updates

---
+++ precache-manifest.2551ce3dc3c260ef3aa6fcd2bd911395.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02f3d6e1542f4b3ded4ef81628f7cc2",
+    "url": "/achmadfauzi.github.io/index.html"
+  },
+  {
+    "revision": "12f4d2df13c2610821f1",
+    "url": "/achmadfauzi.github.io/static/css/main.ce51051c.chunk.css"
+  },
+  {
+    "revision": "4f36ef1f2cd4d8bfe6d3",
+    "url": "/achmadfauzi.github.io/static/js/2.8a2114e3.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/achmadfauzi.github.io/static/js/2.8a2114e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f4d2df13c2610821f1",
+    "url": "/achmadfauzi.github.io/static/js/main.425c12a5.chunk.js"
+  },
+  {
+    "revision": "b1b34c57895beee84c06",
+    "url": "/achmadfauzi.github.io/static/js/runtime-main.22d65210.js"
+  },
+  {
+    "revision": "6250c1e49ce130b510ace4ed90f3f61a",
+    "url": "/achmadfauzi.github.io/static/media/item10.6250c1e4.jpg"
+  },
+  {
+    "revision": "abc21d69d55998dee912ada478651d19",
+    "url": "/achmadfauzi.github.io/static/media/logo1.abc21d69.jpg"
+  }
+]);
